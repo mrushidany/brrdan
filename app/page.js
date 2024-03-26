351346c153b91f2c96efbd8b15ea6c112e4f4a6c
Dany Mrushi <mrushidany@gmail.com>
2024-03-26 13:25:38 +0300

    Removed the image first

--- app/page.js
+++ app/page.js
@@ -3,10 +3,10 @@ import Footer from './_ui/components/Footer'
 export default function Home() {
   return (
     <main className='w-full h-screen flex flex-col pb-4 bg-black-default'>
-      <div className='w-full h-full lg:h-[90%] bg-cover lg:bg-contain bg-center bg-no-repeat' style={{ backgroundImage: `url('/images/traced-brrrdan.jpeg')` }}></div>
+      {/* <div className='w-full h-full lg:h-[90%] bg-cover lg:bg-contain bg-center bg-no-repeat' style={{ backgroundImage: `url('/images/traced-brrrdan.jpeg')` }}></div>
       <div className='flex justify-center items-end mt-8'>
         <Footer />
-      </div>
+      </div> */}
     </main>
   )
 }
