e7df8527dfdd0ba2deea01c5334a8a90eba9b15a
Dany Mrushi <mrushidany@gmail.com>
2024-03-26 13:24:23 +0300

    Added the background color

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Footer from './_ui/components/Footer'
 
 export default function Home() {
   return (
-    <main className='w-full h-screen flex flex-col pb-4'>
+    <main className='w-full h-screen flex flex-col pb-4 bg-black-default'>
       <div className='w-full h-full lg:h-[90%] bg-cover lg:bg-contain bg-center bg-no-repeat' style={{ backgroundImage: `url('/images/traced-brrrdan.jpeg')` }}></div>
       <div className='flex justify-center items-end mt-8'>
         <Footer />
