4d903fb3788b75253737fdf1534871f7f1512ca7
Dany Mrushi <mrushidany@gmail.com>
2024-03-26 14:11:54 +0300

    Added the link from the image

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
+import Link from 'next/link'
 import Footer from './_ui/components/Footer'
 
 export default function Home() {
   return (
     <main className='w-full h-screen flex flex-col pb-4 bg-black-default'>
-      <div className='w-full h-full lg:h-[90%] bg-cover lg:bg-contain bg-center bg-no-repeat' style={{ backgroundImage: `url('/images/traced-brrrdan.jpeg')` }}></div>
+      <Link href='/about' className='w-full h-full lg:h-[90%] bg-cover lg:bg-contain bg-center bg-no-repeat cursor-pointer' style={{ backgroundImage: `url('/images/traced-brrrdan.jpeg')` }}></Link>
       <div className='flex justify-center items-end mt-4 lg:mt-8'>
         <Footer />
       </div>
