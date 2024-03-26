7de702bf6d846c9c97efd236c12a4e06e29cd9ad
Dany Mrushi <mrushidany@gmail.com>
2024-03-26 13:29:47 +0300

    Reduced the gap in mobile devices

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className='w-full h-screen flex flex-col pb-4 bg-black-default'>
       <div className='w-full h-full lg:h-[90%] bg-cover lg:bg-contain bg-center bg-no-repeat' style={{ backgroundImage: `url('/images/traced-brrrdan.jpeg')` }}></div>
-      <div className='flex justify-center items-end mt-8'>
+      <div className='flex justify-center items-end mt-4 lg:mt-8'>
         <Footer />
       </div>
     </main>
