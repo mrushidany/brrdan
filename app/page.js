eab71213a4278f9d2477a6f905506eb3e61a18c1
Dany Mrushi <mrushidany@gmail.com>
2024-03-26 11:42:15 +0300

    Added the brrdan image

--- app/page.js
+++ app/page.js
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <main className='w-screen min-h-screen bg-black-default flex items-center justify-center'>
       <div className='flex flex-col gap-10'>
-        <h1 className='text-yellow-100 text-5xl font-extrabold'>BRRDAN <span className='italic'>!!</span></h1>
+        <h1 className='text-yellow-100 text-5xl font-extrabold text-center'>BRRDAN <span className='italic'>!!</span></h1>
+        <div className='w-[520px] h-[460px] bg-cover bg-center bg-no-repeat' style={{ backgroundImage: `url('/images/brrdan.jpeg')` }}>
+
+        </div>
       </div>
     </main>
   )
