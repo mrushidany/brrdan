1bc2c7d30a41347c96d0469e1607ca02e7317af9
Dany Mrushi <mrushidany@gmail.com>
2024-03-26 11:48:38 +0300

    Changed the background image

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <main className='w-screen min-h-screen bg-black-default flex items-center justify-center'>
       <div className='flex flex-col gap-10'>
         <h1 className='text-yellow-100 text-5xl font-extrabold text-center'>BRRDAN <span className='italic'>!!</span></h1>
-        <div className='w-[520px] h-[460px] bg-cover bg-center bg-no-repeat' style={{ backgroundImage: `url('/images/brrdan.jpeg')` }}>
+        <div className='w-[550px] h-[560px] bg-cover bg-center bg-no-repeat' style={{ backgroundImage: `url('/images/brrrdan.jpeg')` }}>
 
         </div>
       </div>
