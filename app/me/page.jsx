24c436439038f693d3feb9984f88af6a4776d79a
Dany Mrushi <mrushidany@gmail.com>
2024-03-26 15:44:31 +0300

    Responsive design

--- app/me/page.jsx
+++ app/me/page.jsx
@@ -57,20 +57,20 @@ export default function AboutMePage() {
     ]
 
   return (
-    <div className='w-full min-h-screen bg-black-default flex items-center justify-center'>
-      <div className='flex flex-col gap-10 text-yellow-100'>
-        <h1 className='text-5xl font-extrabold text-center cursor-pointer' onClick={() => router.push('/')}>BRRDAN &nbsp;<span className='italic'>!!</span></h1>
-        <ul className='flex flex-row gap-20 text-xl uppercase justify-center'>
+    <div className='w-full min-h-screen py-10 px-4 lg:py-0 lg:px-0 bg-black-default flex items-center justify-center'>
+      <div className='flex flex-col gap-8 lg:gap-10 text-yellow-100'>
+        <h1 className='text-2xl lg:text-5xl font-extrabold text-center cursor-pointer' onClick={() => router.push('/')}>BRRDAN &nbsp;<span className='italic'>!!</span></h1>
+        <ul className='flex flex-row gap-8 lg:gap-20 text-xs lg:text-xl uppercase justify-center'>
             {tabList?.map((item, index) => (
                 <li className={`cursor-pointer ${tab[item.tab] ? 'underline tracking-wider': ''}`} key={index} onClick={() => handleActiveTab(item.tab)}>{item.name}</li>
             ))}
         </ul>
         {tab.music && (
-            <div className='grid grid-cols-3 gap-6'>
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-6'>
                 {music.map((item, index) => (
-                    <div key={index} className={`w-[384px] h-auto flex flex-col gap-3 group hover:cursor-pointer hover:relative`}>
+                    <div key={index} className={`w-full lg:w-[384px] h-auto flex flex-col gap-3 group hover:cursor-pointer hover:relative`}>
                         <div className='w-full h-[260px] bg-cover bg-center bg-no-repeat' style={{ backgroundImage: `url(${item.image})` }}></div>
-                        <h4 className='font-bold text-xl tracking-wider'>{item.title}</h4>
+                        <h4 className='font-bold text-lg lg:text-xl tracking-wider'>{item.title}</h4>
                         <div className='hidden group-hover:block group-hover:absolute bg-black-default w-full h-full bg-opacity-80 group-hover:transition-all ease-in-out duration-500'>
                             <div className='h-full flex items-center justify-center'>
                                 <Link href={item.link} className='w-14 h-14 relative'>
