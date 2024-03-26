95deac8d4bd45a480b3cafb5c37241051a178a2f
Dany Mrushi <mrushidany@gmail.com>
2024-03-26 12:53:52 +0300

    Responsive setup

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Footer from './_ui/components/Footer'
 export default function Home() {
   return (
     <main className='w-full h-screen flex flex-col pb-4'>
-      <div className='w-full h-[90%] bg-contain bg-center bg-no-repeat' style={{ backgroundImage: `url('/images/traced-brrrdan.jpeg')` }}></div>
+      <div className='w-full h-full lg:h-[90%] bg-cover lg:bg-contain bg-center bg-no-repeat' style={{ backgroundImage: `url('/images/traced-brrrdan.jpeg')` }}></div>
       <div className='flex justify-center items-end mt-8'>
         <Footer />
       </div>
