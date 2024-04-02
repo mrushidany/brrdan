f51aa2f185f682cc02596058e07e3ca078b26907
Dany Mrushi <mrushidany@gmail.com>
2024-04-02 18:01:14 +0300

    Explosion setup

--- app/page.js
+++ app/page.js
@@ -2,29 +2,44 @@
 
 import Link from 'next/link'
 import Footer from './_ui/components/Footer'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Image from 'next/image'
-import gun from '../public/images/gunny.png'
+import explosion from '../public/images/explosion.gif'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
   const [pop, setPop] = useState(false)
+
+  const router = useRouter()
+
+  const navigate = () => {
+    setPop(true)
+  }
+
+  useEffect(() => {
+    if (pop) {
+      const timeout = setTimeout(() => {
+        router.push('/me')
+      }, 500)
+      return () => clearTimeout(timeout)
+    }
+  }, [pop, router])
+
   return (
     <main className='w-full h-screen flex flex-col gap-4 pb-4 bg-black-default relative'>
       <div className='w-full h-full lg:h-[90%] bg-cover lg:bg-contain bg-center bg-no-repeat cursor-pointer' style={{ backgroundImage: `url('/images/traced-brrrdan.jpeg')` }}></div>
-      <div className='flex justify-center -mt-8'>
-        <div className='bg-cover bg-no-repeat w-48 h-32' style={{  backgroundImage: `url(/images/gunny.png)` }}>
-          
-        </div>
+      <div className='flex justify-center -mt-4'>
+        <div onClick={navigate} className='bg-cover bg-no-repeat w-48 h-32 cursor-pointer' style={{  backgroundImage: `url(/images/gunny.png)` }}></div>
       </div>
-      <div className='flex justify-center items-end -mt-6'>
+      <div className='flex justify-center items-end -mt-4'>
         <Footer />
       </div>
       {pop && (
         <>
-          <div className='fixed w-full h-full bg-[#002339] opacity-95 inset-0 z-50 flex items-center justify-center'>
-            <Link href='/me'>
-              <Image src={gun} className='' />
-            </Link>
+          <div className='fixed w-full h-full bg-[#151515] inset-0 z-50 flex items-center justify-center'>
+            <div>
+                <Image src={explosion} />
+            </div>
           </div>
         </>
       )}
